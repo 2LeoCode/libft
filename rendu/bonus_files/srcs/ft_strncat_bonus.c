c335a2f1f71b59287c7336518a9bdf9fa6bedf72
2LeoCode <leo.13suardi@outlook.com>
2020-04-30 20:37:57 +0200

    fix-strncat

--- rendu/bonus_files/srcs/ft_strncat_bonus.c
+++ rendu/bonus_files/srcs/ft_strncat_bonus.c
@@ -6,7 +6,7 @@
 /*   By: lsuardi <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/23 18:23:50 by lsuardi           #+#    #+#             */
-/*   Updated: 2020/04/29 22:07:48 by lsuardi          ###   ########.fr       */
+/*   Updated: 2020/04/30 17:25:42 by lsuardi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,18 @@
 
 char	*ft_strncat(char *s1, const char *s2, size_t n)
 {
+	size_t	i;
+
+	i = 0;
 	if (!*s1)
-		return (ft_strncpy(s1, s2, n));
+	{
+		while (s2[i] && i < n)
+		{
+			s1[i] = s2[i];
+			i++;
+		}
+		s1[i] = 0;
+		return (s1);
+	}
 	return (ft_strncat(s1 + 1, s2, n) - 1);
 }
